191f3dc55a92f5623c1b17ef419a24866e3da24b
Danielle Cushing <dcushing09@gmail.com>
2019-12-26 10:22:12 -0600

    set up project in xcode

---
+++ ShowMeTheWorld/ShowMeTheWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShowMeTheWorld
+//
+//  Created by Danielle Cushing on 12/26/19.
+//  Copyright © 2019 Danielle Cushing. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
