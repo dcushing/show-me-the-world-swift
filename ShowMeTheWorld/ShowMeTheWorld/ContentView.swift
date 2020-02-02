556c1cccf5e34b4c54b9fa0abca38c278282fa95
Danielle Cushing <dcushing09@gmail.com>
2020-02-02 12:42:06 -0600

    added button to initial screen

--- ShowMeTheWorld/ShowMeTheWorld/ContentView.swift
+++ ShowMeTheWorld/ShowMeTheWorld/ContentView.swift
@@ -4,13 +4,26 @@
 //
 //  Created by Danielle Cushing on 12/26/19.
 //  Copyright © 2019 Danielle Cushing. All rights reserved.
-//
+//  Note: press Opt + Cmd + P to make preview start again if it stops
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        // button styling help from https://www.appcoda.com/swiftui-buttons/
+            Button(action: {
+                print("button was clicked")
+            }) {
+                Text("Show Me The World")
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                .padding() // 1. add padding
+                .background(LinearGradient(gradient: Gradient(colors: [Color("DarkGreen"), Color("LightGreen")]), startPoint: .leading, endPoint: .trailing))
+                .cornerRadius(30)
+                    .padding(.horizontal, 20)
+                .foregroundColor(.white) // 3. set foreground/font color to white
+                .font(.title) // 4. change font type
+
+            }
     }
 }
 
